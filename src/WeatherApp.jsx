9cfe8c5218e30e3d04fe7a9d42ef374db29ec6b1
EnzoMelgarejo <enzomelgarejo@gmail.com>
2025-02-28 17:50:34 -0300

    Actualizacion de comentarios y README

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,3 +1,20 @@
+/**
+ * Componente WeatherApp
+ * 
+ * Esta aplicación permite al usuario consultar el clima de una ciudad ingresada. 
+ * Utiliza la API de OpenWeatherMap para obtener datos meteorológicos en tiempo real.
+ * 
+ * Funcionalidades:
+ * - Permite al usuario ingresar el nombre de una ciudad en un campo de texto.
+ * - Realiza una solicitud a la API de OpenWeatherMap cuando el usuario envía el formulario.
+ * - Muestra el nombre de la ciudad, la temperatura en grados Celsius y la descripción del clima.
+ * - Muestra un ícono representativo del estado del clima actual.
+ * 
+ * Tecnologías utilizadas:
+ * - React y el hook useState para manejar el estado.
+ * - Fetch API para realizar la solicitud HTTP.
+ */
+
 import { useState } from "react"
 
 export const WeatherApp = () => {
@@ -11,9 +28,11 @@ export const WeatherApp = () => {
     const fetchWeather = async() => {
 
         try {
-        const response= await fetch (`${urlBase}?q=${city}&appid=${APIKey}`)
-        const data= await response.json()
-        setDataWeather(data)
+
+            const response= await fetch (`${urlBase}?q=${city}&appid=${APIKey}`)
+            const data= await response.json()
+            setDataWeather(data)
+
         }catch (error){
             console.error("ocurrio un error", error)
         }
@@ -25,8 +44,10 @@ export const WeatherApp = () => {
     }
 
     const handleSubmit = (e) => {
+
         e.preventDefault()
         if(city.length > 0) fetchWeather()
+            
     }
 
   return (
